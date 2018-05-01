2e9d41feb383cee09e0b87b7ec5af8433b9acc35
Mariana Trevisoli Gervino <mariantr@amdocs.com>
2018-05-01 19:30:51 -0300

    This commit adds a test to Rafa test repository

---
+++ studying/src/CalculaNota.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class CalculaNota {
+
+	public static void primeFactors(long n)
+    {
+        // Print the number of 2s that divide n
+        while (n%2==0)
+        {
+            System.out.print(2 + " ");
+            n /= 2;
+        }
+ 
+        // n must be odd at this point.  So we can
+        // skip one element (Note i = i +2)
+        for (long i = 3; i <= Math.sqrt(n); i+= 2)
+        {
+            // While i divides n, print i and divide n
+            while (n%i == 0)
+            {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+ 
+        // This condition is to handle the case whien
+        // n is a prime number greater than 2
+        if (n > 2)
+            System.out.print(n);
+        }
+	
+	public static void main(String[] args) {
+		long n = 18;
+		primeFactors(n);
+	}
+}
